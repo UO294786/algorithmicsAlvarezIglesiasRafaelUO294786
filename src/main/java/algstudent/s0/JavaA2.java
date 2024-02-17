8e5df194f63679937eb9435712a6b10b684616d7
rafae <rafae@SALON>
2024-02-17 02:13:51 +0100

    Session 0 work

---
+++ src/main/java/algstudent/s0/JavaA2.java
@@ -0,0 +1,49 @@
+package algstudent.s0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaA2 {
+	final static int NUM_OF_CASES = 7;
+	
+	
+	public static void main(String[] args) {
+		long t1,t2;
+		int n = 10000;
+		List<Integer> primes = new ArrayList<>();
+		
+		for (int i = 0; i < NUM_OF_CASES; i++) {
+			t1 = System.currentTimeMillis();
+			
+			primes = primesList( n );
+			
+			t2 = System.currentTimeMillis();
+			
+			System.out.println("n = " + n + " *** " + "time = " + (t2 - t1) + " milliseconds");		
+			
+			n *= 2;
+		}
+
+	}
+
+
+	private static List<Integer> primesList(int n) {
+		List<Integer> primes = new ArrayList<>();
+		
+		for (int i = 2; i <= n; i++)
+			if (isPrime( i ))
+				primes.add( i );
+		
+		return primes;
+	}
+
+
+	private static boolean isPrime(int n) {
+		for (int i = 2; i < n; i++)
+			if (n % i == 0)
+				return false;
+		
+		return true;
+	}
+
+}
--- src/main/java/algstudent/s0/Math.java
+++
@@ -1,7 +0,0 @@
-package algstudent.s0;
-
-public class Math {
-	public int sum(int a, int b) {
-		return a + b ;
-	}
-}
