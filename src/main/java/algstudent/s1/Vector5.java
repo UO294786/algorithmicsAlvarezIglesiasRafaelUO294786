ad9e48a62dd00aac893c4662b30e4681958c00da
rafae <rafae@SALON>
2024-02-17 21:44:31 +0100

    Finished measurements

--- src/main/java/algstudent/s1/Vector5.java
+++ src/main/java/algstudent/s1/Vector5.java
@@ -5,25 +5,25 @@ package algstudent.s1;
  * the size of the problem. In addition, we use a repetition value 
  * determined by nTimes, an argument of the program
  */
-public class Vector4 {
-	static int[]v;
+public class Vector5 {
+	static int[]v, res;
 	
 	public static void main(String arg []) {
 		int repetitions = Integer.parseInt(arg[0]);
 		long t1,t2;
-		int sum = 0;
 		
 		for (int n=10000; n<=Integer.MAX_VALUE; n*=2){ //n is increased *2  
 			  v = new int[n];
+			  res = new int[n];
 			  Vector1.fillIn(v);
 			  
 			  t1 = System.currentTimeMillis();
 			  //We have to repeat the whole process to be measured
 			  for (int repetition=1; repetition<=repetitions; repetition++){    	
-			     sum = Vector1.sum(v); //Sum is executed in O(n)
+			     Vector1.maximum(v, res); //Max is executed in O(n)
 			  }
 			  t2 = System.currentTimeMillis();
-			  System.out.printf("SIZE=%d TIME=%d milliseconds SUM=%d NTIMES=%d\n", n, t2-t1, sum, repetitions);	
+			  System.out.printf("SIZE=%d TIME=%d milliseconds MAX=%d NTIMES=%d\n", n, t2-t1, res[1], repetitions);	
 		}//for 
 		
 	}//main
--- src/main/java/algstudent/s1/Vector7.java
+++ src/main/java/algstudent/s1/Vector7.java
@@ -5,25 +5,27 @@ package algstudent.s1;
  * the size of the problem. In addition, we use a repetition value 
  * determined by nTimes, an argument of the program
  */
-public class Vector4 {
-	static int[]v;
+public class Vector7 {
+	static int[]v, b;
 	
 	public static void main(String arg []) {
 		int repetitions = Integer.parseInt(arg[0]);
 		long t1,t2;
-		int sum = 0;
+		int matches = 0;
 		
 		for (int n=10000; n<=Integer.MAX_VALUE; n*=2){ //n is increased *2  
 			  v = new int[n];
+			  b = new int[n];
 			  Vector1.fillIn(v);
+			  Vector1.fillIn(b);
 			  
 			  t1 = System.currentTimeMillis();
 			  //We have to repeat the whole process to be measured
 			  for (int repetition=1; repetition<=repetitions; repetition++){    	
-			     sum = Vector1.sum(v); //Sum is executed in O(n)
+			     matches = Vector1.matches2(v, b); //Matches2 is executed in O(n)
 			  }
 			  t2 = System.currentTimeMillis();
-			  System.out.printf("SIZE=%d TIME=%d milliseconds SUM=%d NTIMES=%d\n", n, t2-t1, sum, repetitions);	
+			  System.out.printf("SIZE=%d TIME=%d milliseconds MATCHES=%d NTIMES=%d\n", n, t2-t1, matches, repetitions);	
 		}//for 
 		
 	}//main
