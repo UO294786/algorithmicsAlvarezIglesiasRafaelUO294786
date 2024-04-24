549b27d5e83404427ad4421c47e909d2cd8c1330
UO294786 <uo294786@uniovi.es>
2024-04-24 16:51:30 +0200

    Classwork

--- src/main/java/algstudent/s6/NumericalSquareOneTimes.java
+++ src/main/java/algstudent/s6/NumericalSquareOneTimes.java
@@ -9,7 +9,7 @@ public class NumericalSquareOneTimes {
 		
 		for (int n = 0; n <= 7; n++) {
 			String path = "src/main/java/algstudent/s6/tests/test0" + String.valueOf(n) + ".txt";
-			System.out.println("Test0" + n + ": ");
+		 	System.out.println("Test0" + n + ": ");
 			t1 = System.currentTimeMillis();
 			
 			sq.solve(path);
